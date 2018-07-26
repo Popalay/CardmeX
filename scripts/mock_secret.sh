8883350e96a5b2f7ca7f454b628b8519add1d31a
Denis Nikiforov <popalay@ex.ua>
2018-07-26 15:19:18 +0300

    - Fix bash script to mock secret info

--- scripts/mock_secret.sh
+++ scripts/mock_secret.sh
@@ -1,1 +1,13 @@
-echo 'object Secret {}' >buildSrc/src/main/java/Secret.kt
+echo 'object Secret {
+    const val googleClientId = null;
+
+    const val debugStoreFile = null;
+    const val releaseStoreFile = null;
+    const val storePassword = null;
+
+    const val releaseKeyAlias = null;
+    const val releaseKeyPassword = null;
+
+    const val releaseXKeyAlias = null;
+    const val releaseXKeyPassword = null;
+}' >./buildSrc/src/main/java/Secret.kt
