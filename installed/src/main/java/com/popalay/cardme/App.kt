af73d70b8e28bc4406c07874872bda394798bd06
Denis Nikiforov <popalay@ex.ua>
2018-08-20 12:25:36 +0300

    - Set async main thread scheduler

--- installed/src/main/java/com/popalay/cardme/App.kt
+++ installed/src/main/java/com/popalay/cardme/App.kt
@@ -1,12 +1,15 @@
 package com.popalay.cardme
 
 import android.app.Application
+import android.os.Looper
 import com.crashlytics.android.Crashlytics
 import com.crashlytics.android.core.CrashlyticsCore
 import com.facebook.stetho.Stetho
 import com.popalay.cardme.core.error.DefaultRxErrorHandler
 import com.popalay.cardme.injector.Injector
 import io.fabric.sdk.android.Fabric
+import io.reactivex.android.plugins.RxAndroidPlugins
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.plugins.RxJavaPlugins
 
 class App : Application() {
@@ -15,7 +18,13 @@ class App : Application() {
         super.onCreate()
         Injector.inject(this)
         initDevTools()
+        initRx()
+    }
+
+    private fun initRx() {
         RxJavaPlugins.setErrorHandler(DefaultRxErrorHandler())
+        val asyncMainThreadScheduler = AndroidSchedulers.from(Looper.getMainLooper(), true)
+        RxAndroidPlugins.setInitMainThreadSchedulerHandler { asyncMainThreadScheduler }
     }
 
     private fun initDevTools() {
