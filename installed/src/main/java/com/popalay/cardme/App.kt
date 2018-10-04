dabb24dcc0a9f59ba6886868fc9550dfda0d0d6b
popalay <popalay1@gmail.com>
2018-10-04 17:40:10 +0300

    - Inc app version - Disable Crashlytics because the R8 cut it

--- installed/src/main/java/com/popalay/cardme/App.kt
+++ installed/src/main/java/com/popalay/cardme/App.kt
@@ -2,12 +2,9 @@ package com.popalay.cardme
 
 import android.app.Application
 import android.os.Looper
-import com.crashlytics.android.Crashlytics
-import com.crashlytics.android.core.CrashlyticsCore
 import com.facebook.stetho.Stetho
 import com.popalay.cardme.core.error.DefaultRxErrorHandler
 import com.popalay.cardme.injector.Injector
-import io.fabric.sdk.android.Fabric
 import io.reactivex.android.plugins.RxAndroidPlugins
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.plugins.RxJavaPlugins
@@ -28,10 +25,12 @@ class App : Application() {
     }
 
     private fun initDevTools() {
+/*
         val crashlyticsKit = Crashlytics.Builder()
             .core(CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build())
             .build()
         Fabric.with(this, crashlyticsKit)
+*/
 
         if (BuildConfig.DEBUG) {
             Stetho.initializeWithDefaults(this)
