a9677813437bfa57ba6790db725987dfd36ec147
popalay <popalay1@gmail.com>
2018-10-08 15:58:16 +0300

    - Create cloud-function to observe add card request

--- firebase/cloud-function/functions/src/index.ts
+++ firebase/cloud-function/functions/src/index.ts
@@ -1,8 +1,23 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+admin.initializeApp(functions.config().firebase);
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.sendPushWhenAddCardRequest = functions.firestore
+    .document('request/{requestId}')
+    .onCreate((snap, context) => {
+
+      const request = snap.data();
+
+      const notification = {
+        type: "ADD_CARD",
+        title: "Title",
+        description: "Description",
+        card: request.card
+    };
+
+      // save notification
+      console.log('Write new notification');
+      return admin.firestore().collection('notification').add(notification).then(() => {
+        console.log('Write new notification succeeded!');
+      });
+    });
