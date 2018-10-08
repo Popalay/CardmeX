0a8922ec3b490326dad88476be4d03e3b32dea2b
popalay <popalay1@gmail.com>
2018-10-09 00:24:29 +0300

    - Create Cloud function to send a push notification

--- firebase/cloud-function/functions/src/index.ts
+++ firebase/cloud-function/functions/src/index.ts
@@ -1,23 +1,56 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+
 admin.initializeApp(functions.config().firebase);
 
 exports.sendPushWhenAddCardRequest = functions.firestore
-    .document('request/{requestId}')
-    .onCreate((snap, context) => {
+  .document('request/{requestId}')
+  .onCreate((snap, context) => {
 
-      const request = snap.data();
+    const request = snap.data();
+    const toUuid = request.toUuid
+    const fromUuid = request.fromUuid
 
-      const notification = {
-        type: "ADD_CARD",
-        title: "Title",
-        description: "Description",
-        card: request.card
-    };
+    return admin.auth().getUser(fromUuid)
+      .then((userFrom) => {
+        console.log('User has fetched');
 
-      // save notification
-      console.log('Write new notification');
-      return admin.firestore().collection('notification').add(notification).then(() => {
-        console.log('Write new notification succeeded!');
+        const payload = {
+          data: {
+            //token: token
+          },
+          notification: {
+            title: "Title",
+            body: "Description",
+            sound: "default"
+          },
+        };
+
+        const options = {
+          priority: "high",
+          timeToLive: 60 * 60 * 24
+        };
+
+        const notificationRef = admin.firestore().collection('notification').doc()
+        const notification = {
+          id: notificationRef.id,
+          type: "ADD_CARD",
+          title: userFrom.displayName + 'wants add your card',
+          description: "Description",
+          card: request.card
+        };
+
+        const saveNotificationPromise = notificationRef.set(notification)
+        const sendPushPromise = admin.firestore().collection("token").where('userUuid', '==', toUuid).limit(1).get()
+          .then(token => admin.messaging().sendToDevice(token.docs[0].data().token, payload, options))
+
+        return Promise.all([saveNotificationPromise, sendPushPromise])
+          .then(results => {
+            console.log('Notification has added');
+            console.log('Push has sent');
+          })
+          .catch(error => {
+            console.log('Send notification error: ', error);
+          });
       });
-    });
+  });
