da72d64405accac7f3177855c372c60276b7386f
popalay <popalay1@gmail.com>
2018-11-01 17:05:21 +0200

    - Update AGP to 3.3-beta02

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -8,7 +8,7 @@ object Versions {
     const val targetSdkVersion = 28
     const val compileSdkVersion = 28
 
-    const val gradlePlugin = "3.3.0-beta01"
+    const val gradlePlugin = "3.3.0-beta02"
     const val kotlinLanguage = "1.3.0"
 
     const val constraintLayout = "2.0.0-alpha2"
