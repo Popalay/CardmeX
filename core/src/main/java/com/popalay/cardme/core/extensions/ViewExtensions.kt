aad0a0ccf261193e2b175382896bf2c99ab4b08d
popalay <popalay1@gmail.com>
2018-11-13 16:10:33 +0200

    - Fix card editing

--- core/src/main/java/com/popalay/cardme/core/extensions/ViewExtensions.kt
+++ core/src/main/java/com/popalay/cardme/core/extensions/ViewExtensions.kt
@@ -3,9 +3,7 @@ package com.popalay.cardme.core.extensions
 import android.widget.EditText
 
 fun EditText.setTextIfNeeded(value: String?) {
-    if (value.isNullOrBlank()) {
-        text = null
-    } else if (text.length != value.length || text.toString() != value) {
-        text.replaceRange(0 until text.length, value)
+    if (text.length != value?.length || text.toString() != value) {
+        text.replace(0, text.length, value)
     }
 }
